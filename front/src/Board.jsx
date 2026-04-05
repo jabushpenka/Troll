2e861f9ef134f09e9b4c54474176186f19e68ee1
mainlame <timurtananusko@gmail.com>
2026-04-05 22:33:01 +0700

    1. микро-фикс съехваших названий карточек 2. перенес большинство элементов из board.jsx в отдельные функции

--- front/src/Board.jsx
+++ front/src/Board.jsx
@@ -212,6 +212,151 @@ export default function Board() {
       return;
     }
   };
+  
+  //элементы
+  
+  const columnTitle = (col) => {
+    return (
+      (updateActiveColumn.colId == col.id) 
+        ? (
+          <div>
+            <input
+              autoFocus
+              value={updateActiveColumn.title}
+              onChange={(e) => setUpdateActiveColumn(prev => ({...prev, title: e.target.value}))}
+              onKeyDown={(e) => updateColumnKeyPress(e)} 
+              onBlur={() => {
+                if (updateActiveColumn.title.trim()){
+                  updateColumnTitle(col.id, updateActiveColumn.title)
+                }
+                else{
+                  setUpdateActiveColumn({colId: null, title: ""})
+                }
+              }}
+            />
+            <button className="updateColumnName" onMouseDown={
+              (e) => {
+                e.preventDefault(); 
+                updateColumnTitle(col.id,updateActiveColumn.title);
+              }}>
+            Изменить</button>
+          </div>
+          )
+        : <h2 onClick={() => {setUpdateActiveColumn({colId: col.id, title: col.title})}}>{col.title}</h2>
+      )
+  }
+
+  const cardTitle = (col, card) => {
+    return (
+      (updateActiveCard.colId == col.id) && (updateActiveCard.cardId == card.id) 
+        ? (
+          <div>
+            <input
+              autoFocus
+              value={updateActiveCard.title}
+              onChange={(e) => setUpdateActiveCard(prev => ({...prev, title: e.target.value}))}
+              onKeyDown={(e) => updateCardKeyPress(e)} 
+              onBlur={() => {
+                if (updateActiveCard.title.trim()){
+                  updateCardTitle(colId, card.id, updateActiveCard.title)
+                }
+                else{
+                  setUpdateActiveCard({colId: null, cardId: null, title: ""})
+                }
+              }}
+            />
+            <button className="updateCardName" onMouseDown={
+              (e) => {
+                e.preventDefault(); 
+                updateCardTitle(col.id,card.id,updateActiveCard.title);
+              }}>
+            Изменить</button>
+          </div>
+        )
+        : <h2 onClick={() => {setUpdateActiveCard({colId: col.id, cardId: card.id, title: card.title})}}>{card.title}</h2>
+      )
+  }
+
+  const cardAddButton = (col) => {
+    return (
+      (activeCard.colId === col.id)
+        ? (
+            <div className="addCardContainer">
+              <input
+                autoFocus
+                value={activeCard.title}
+                onChange={(e) => setActiveCard(prev => ({...prev, title: e.target.value}))}
+                onKeyDown={(e) => addCardKeyPress(e,col.id)}
+                placeholder="Название карточки"
+                onBlur={() => {
+                  if (activeCard.title.trim()){
+                    addCard(col.id)
+                  }
+                  else{
+                    setActiveCard({colId: null, title: ""})
+                  }
+                }}
+              />
+              <button className="addcard" onMouseDown={(e) => {e.preventDefault(); addCard(col.id)}}>Добавить</button>
+            </div>
+          ) 
+          : (
+            <button 
+              onClick={() => setActiveCard({colId: col.id, title: ""})}>
+                <img src={plus}/>
+                Карточка
+            </button>
+          )
+      )
+  }
+    
+  const taskList = (col, card) => {
+    return (
+      <ul className="tasklist">
+        {card.tasks.map(task => (
+          <li key={task.id}>
+            <button 
+              onClick={() => toggleTask(col.id, card.id, task.id)}>
+                <img src={task.done ? taskbuttondone : taskbutton}/>
+            </button>
+            <span 
+              className={task.done ? "done" : ""}
+              onClick={() => toggleTask(col.id, card.id, task.id)}>
+                {task.text}
+            </span>
+          </li>
+        ))}
+      </ul>
+    )
+  }
+ 
+  const taskAddButton = (colId, cardId) => {
+    return (
+    (activeTask.colId === colId) && (activeTask.cardId === cardId) ? (
+      <div>
+        <input
+          autoFocus
+          value={activeTask.title}
+          onChange={(e) => setActiveTask(prev => ({...prev, title: e.target.value}))}
+          onKeyDown={(e) => addTaskKeyPress(e,colId,cardId)}
+          onBlur={() => {
+            if (activeTask.title.trim()){
+              addTask(colId,cardId)
+            }
+            else {
+              setActiveTask({colId: null, cardId: null, title: ""})
+            }
+          }}
+          placeholder="Task"
+        />
+        <button className="addtask" onMouseDown={(e) => {e.preventDefault(); addTask(colId, cardId)}}>Добавить</button>
+      </div>
+    ) : (
+      <button onClick={() => setActiveTask({ colId: colId, cardId: cardId, title: ""})}>
+        <img src={add}/> добавить задание
+      </button>
+    ))
+  }
 
   return (
     <div className="board">
@@ -237,135 +382,20 @@ export default function Board() {
         {boardData.columns.map(col => (
           <div key={col.id} className="column">
             <div className="columnheader">
-              { (updateActiveColumn.colId == col.id) 
-                ? (
-                  <div>
-                    <input
-                      autoFocus
-                      value={updateActiveColumn.title}
-                      onChange={(e) => setUpdateActiveColumn(prev => ({...prev, title: e.target.value}))}
-                      onKeyDown={(e) => updateColumnKeyPress(e)} 
-                      onBlur={() => {
-                        if (updateActiveColumn.title.trim()){
-                          updateColumnTitle(col.id, updateActiveColumn.title)
-                        }
-                        else{
-                          setUpdateActiveColumn({colId: null, title: ""})
-                        }
-                      }}
-                    />
-                    <button className="updateColumnName" onMouseDown={
-                      (e) => {
-                        e.preventDefault(); 
-                        updateColumnTitle(col.id,updateActiveColumn.title);
-                      }}>
-                    Изменить</button>
-                  </div>
-                )
-                : <h2 onClick={() => {setUpdateActiveColumn({colId: col.id, title: col.title})}}>{col.title}</h2>
-              }
+              {columnTitle(col)}
               <button onClick={() => removeColumn(col.id)}><img src={trashbold}/></button>
             </div>
             {col.cards.map(card => (
               <div key={card.id} className="card">
                 <div className="cardheader">
-                  { (updateActiveCard.colId == col.id) && (updateActiveCard.cardId == card.id) 
-                  ? (
-                    <div>
-                      <input
-                        autoFocus
-                        value={updateActiveCard.title}
-                        onChange={(e) => setUpdateActiveCard(prev => ({...prev, title: e.target.value}))}
-                        onKeyDown={(e) => updateCardKeyPress(e)} 
-                        onBlur={() => {
-                          if (updateActiveCard.title.trim()){
-                            updateCardTitle(col.id, card.id, updateActiveCard.title)
-                          }
-                          else{
-                            setUpdateActiveCard({colId: null, cardId: null, title: ""})
-                          }
-                        }}
-                      />
-                      <button className="updateCardName" onMouseDown={
-                        (e) => {
-                          e.preventDefault(); 
-                          updateCardTitle(col.id,card.id,updateActiveCard.title);
-                        }}>
-                      Изменить</button>
-                    </div>
-                  )
-                  : <h2 onClick={() => {setUpdateActiveCard({colId: col.id, cardId: card.id, title: card.title})}}>{card.title}</h2>
-                }
+                  {cardTitle(col,card)}
                   <button onClick={() => removeCard(col.id,card.id)}><img src={trashregular}/></button>
                 </div>
-                {(activeTask.colId === col.id) && (activeTask.cardId === card.id) ? (
-                  <div>
-                    <input
-                      autoFocus
-                      value={activeTask.title}
-                      onChange={(e) => setActiveTask(prev => ({...prev, title: e.target.value}))}
-                      onKeyDown={(e) => addTaskKeyPress(e,col.id,card.id)}
-                      onBlur={() => {
-                        if (activeTask.title.trim()){
-                          addTask(col.id,card.id)
-                        }
-                        else {
-                          setActiveTask({colId: null, cardId: null, title: ""})
-                        }
-                      }}
-                      placeholder="Task"
-                    />
-                    <button className="addtask" onMouseDown={(e) => {e.preventDefault(); addTask(col.id, card.id)}}>Добавить</button>
-                  </div>
-                ) : (
-                  <button onClick={() => setActiveTask({ colId: col.id, cardId: card.id, title: ""})}>
-                    <img src={add}/> добавить задание
-                  </button>
-                )}
-
-                <ul className="tasklist">
-                  {card.tasks.map(task => (
-                    <li key={task.id}>
-                      <button 
-                        onClick={() => toggleTask(col.id, card.id, task.id)}>
-                          <img src={task.done ? taskbuttondone : taskbutton}/>
-                      </button>
-                      <span 
-                        className={task.done ? "done" : ""}
-                        onClick={() => toggleTask(col.id, card.id, task.id)}>
-                          {task.text}
-                      </span>
-                    </li>
-                  ))}
-                </ul>
+                  {taskAddButton(col.id,card.id)}
+                {taskList(col,card)}
               </div>
             ))}
-            {activeCard.colId === col.id ? (
-                <div className="addCardContainer">
-                  <input
-                    autoFocus
-                    value={activeCard.title}
-                    onChange={(e) => setActiveCard(prev => ({...prev, title: e.target.value}))}
-                    onKeyDown={(e) => addCardKeyPress(e,col.id)}
-                    placeholder="Название карточки"
-                    onBlur={() => {
-                      if (activeCard.title.trim()){
-                        addCard(col.id)
-                      }
-                      else{
-                        setActiveCard({colId: null, title: ""})
-                      }
-                    }}
-                  />
-                  <button className="addcard" onMouseDown={(e) => {e.preventDefault(); addCard(col.id)}}>Добавить</button>
-                </div>
-              ) : (
-                <button 
-                  onClick={() => setActiveCard({colId: col.id, title: ""})}>
-                    <img src={plus}/>
-                    Карточка
-                </button>
-              )}
+            {cardAddButton(col)}
           </div>
         ))}
       </div>
